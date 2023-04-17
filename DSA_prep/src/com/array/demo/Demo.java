11cb7e8a55c123041f29ec4a99ab9b6bc67f02c5
Yarra Raviteja <yraviteja125@gmail.com>
2023-04-17 09:23:09 +0530

    basic commit_3

--- DSA_prep/src/com/array/demo/Demo.java
+++ DSA_prep/src/com/array/demo/Demo.java
@@ -3,9 +3,7 @@ package com.array.demo;
 public class Demo {
 
 	public static void main(String[] args) {
-
-		
-		System.out.println(" hello world");
+		System.out.println(" hello world 123");
 
 	}
 
